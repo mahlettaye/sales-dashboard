b1cfec3ba43eb7e080610d5ea65a42e350efef2f
mahlettaye <you@example.com>
2021-10-10 11:49:13 +0300

    database.py added

---
+++ api/databse.py
@@ -0,0 +1,40 @@
+## Connecting to the database
+
+## importing 'mysql.connector' as mysql for convenient
+import mysql.connector as mysql
+
+
+
+class DatabaseHandelr:
+    """ ## connecting to the database using 'connect()' method
+        ## it takes 3 required parameters 'host', 'user', 'passwd'
+    """
+    def db_connect(self):
+        try:
+            db = mysql.connect(
+            host = "localhost",
+            user = "root",
+            passwd = ""
+            )
+
+            print(db) #
+        except Exception as e :
+            print ("Unable to connet to Mysqlpass"+e)
+         
+        return db
+    def create_database (self, conn):
+        try:
+            ## creating an instance of 'cursor' class which is used to execute the 'SQL' statements in 'Python'
+            cursor = conn.cursor()
+
+            ## creating a databse called 'datacamp'
+            ## 'execute()' method is used to compile a 'SQL' statement
+            ## below statement is used to create tha 'datacamp' database
+            cursor.execute("CREATE DATABASE IF NOT EXISTS Rossmann")
+        except Exception as e:
+            print("Unable to create Database"+e)
+            pass
+
+if __name__ == "__main__":
+    dbobj = DatabaseHandelr()
+    dbobj.db_connect()
