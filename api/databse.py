5016ff82ec7b865a92fcf6d33c2615529b979e4b
mahlettaye <you@example.com>
2021-10-12 16:45:33 +0300

    Some modification

--- api/databse.py
+++ api/databse.py
@@ -2,11 +2,12 @@
 
 ## importing 'mysql.connector' as mysql for convenient
 import mysql.connector as mysql
-from data_processor import DataProcessor
+from api.data_processor import DataProcessor
 from pandas.io import sql
 from sqlalchemy import create_engine
 import json 
 import collections
+from datetime import datetime,date
 
 
 
@@ -52,10 +53,10 @@ class DatabaseHandelr:
             cursor = db.cursor()
 
             ## creating a table called 'sales' in the 'Rossmann' database   
-            cursor.execute("""CREATE TABLE IF NOT EXISTS rsales (Store INT (5),
+            cursor.execute("""CREATE TABLE IF NOT EXISTS RSales (SalesID  INT (5)NOT NULL AUTO_INCREMENT, Store INT (5),
             DayOfWeek INT(5), Date  INT(5), Sales INT(5), Customers INT(5),
-            Open INT(5), PromoStateHoliday INT(5),  SchoolHoliday INT(5), 
-            Month INT(5),  Day INT(5),  Year INT(5),  Week INT(5))""")
+            Open INT(5), Promo INT(5), StateHoliday INT(5),  SchoolHoliday INT(5), 
+            Month INT(5),  Day INT(5),  Year INT(5),  Week INT(5), PRIMARY KEY (SalesID))""")
         except Exception as e:
          print("unable to create table", e)
     
@@ -64,12 +65,13 @@ class DatabaseHandelr:
         try:
             obj= DataProcessor()
             data= obj.data_to_send()
-            data= data[:100]
+            data = data [:10000]
+           
             
 
             engine = create_engine('mysql://root:@localhost/Rossmann')
             with engine.connect() as conn, conn.begin():
-                data.to_sql('rsales', conn, if_exists='replace')
+                data.to_sql('RSales', conn, if_exists='replace')
             print("All records are submitted successfully")
         except Exception as e:
             print("unable to insert data", e)
@@ -83,7 +85,7 @@ class DatabaseHandelr:
             )
         
         cur = db.cursor()
-        cur.execute("SELECT Year,Sales FROM rsales where Open !=0")
+        cur.execute("SELECT * FROM RSales LIMIT 2000")
         rows = cur.fetchall()
         rowarray_list = []
         for row in rows:
@@ -95,16 +97,45 @@ class DatabaseHandelr:
         objects_list = []
         for row in rows:
             d = collections.OrderedDict()
-            d["Year"] = row[0]
-            d["Sales"] = row[1]
+            d["Store"] = row[1]
+            d["DayOfWeek"] = row[2]
+            d["Date"] = row[3]
+            d["Sales"] = row[4]
+            d["Customers"] = row[5]
+            d["Open"] = row[6]
+            d["Promo"] = row[7]
+            d["StateHoliday"] = row[8]
+            d["SchoolHoliday"] = row[9]
+            d["Month"] = row[10]
+            d["Day"] = row[11]
+            d["Year"] = row[12]
+            d["Week"] = row[13]
             
             objects_list.append(d)
 
-        j = json.dumps(objects_list)
+        #j = json.dumps(objects_list)
 
-        with open("test.js", "w") as f:
-            f.write(j)
-        return j
+        # with open("test.js", "w") as f:
+        #     f.write(j)
+        return objects_list
+    def json_serial(obj):
+        """JSON serializer for objects not serializable by default json code"""
+
+        if isinstance(obj, (datetime, date)):
+            return obj.isoformat()
+        raise TypeError ("Type %s not serializable" % type(obj))
+    def get_games():
+        db = mysql.connect(
+            host = "localhost",
+            user = "root",
+            passwd = "",
+            database = "Rossmann"
+            )
+       
+        cursor = db.cursor()
+        query = "SELECT Year,Sales FROM rsales where Open !=0"
+        cursor.execute(query)
+        return cursor.fetchall()
         
 
 
@@ -120,6 +151,6 @@ class DatabaseHandelr:
 if __name__ == "__main__":
     dbobj = DatabaseHandelr()
     conn = dbobj.db_connect()
-    #dbobj.create_table()
-    #dbobj.insert_data()
-    dbobj.select_function()
+    dbobj.create_table()
+    dbobj.insert_data()
+    #dbobj.select_function()
